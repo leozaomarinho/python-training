73809644a6b3e16fb42b6e07cd37bad9cd3bd6b2
Leonardo Marinho <81639502+leozaomarinho@users.noreply.github.com>
2023-11-22 22:37:33 -0300

    update files

--- operadores.py
+++ operadores.py
@@ -1,7 +1,23 @@
 num = 2
-num2= 5
+num2= 8
 num3 =8
 
 result = num2 >num or  num2 > num3
 
 print(result)
+
+if num3>num2:
+    print(f'{num3} é maior que {num2}')
+    
+    
+#fimDoIf   
+elif num3==num2:
+    print('Os dois possuem o mesmo valor.')
+
+
+#elseIf em python é assim
+#fimDoElif
+else:
+    print(f'{num3} é menor que {num2}')
+    
+#fimDoElse
