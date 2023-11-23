f6a7db0b29d3f733ea02f0cc2731cb5ef66ccb71
Leonardo Marinho <81639502+leozaomarinho@users.noreply.github.com>
2023-11-22 21:37:35 -0300

    input e output em python

---
+++ heelo.py
@@ -0,0 +1,18 @@
+nome = input("Digite seu nome: ")
+idade = input("Qual a sua idade: ")
+idade = str(idade) #fazendo o casting do valor int para string
+cidade = input("Qual a sua cidade: ")
+
+print(nome)
+print(idade)
+print(cidade)
+
+print('O ' + nome + ' tem '+ idade +' anos de idade, e mora em '+ cidade)
+"""
+Testando o comentario
+esse teste
+"""
+
+anoNascimento = input("Digite o seu ano de nascimento para saber sua idade: ")
+idade = 2023 - anoNascimento
+print(type(idade2023))
