e62145af57c6ca06fe9157384308d56e1cd05c39
Leonardo Marinho <leowalkg6@gmail.com>
2023-12-14 21:39:19 -0300

    finalizando metodos de classe

--- library.py
+++ library.py
@@ -11,5 +11,15 @@ class Library:
             for livro in self._livros:
                 print(livro)
         
-        
     
+    def solicitar_livro(self,solicitante,titulo_livro):
+        for livro in self._livros:
+            if livro.nome == titulo_livro and livro.disponivel == True:
+               if solicitante.livro_pend is None:
+                   solicitante.livro_pend = titulo_livro
+                   livro.disponivel = False
+                   print(f'O {solicitante.nome} solicitou o livro {livro.nome}')
+               else:
+                   print(f'O {solicitante.nome} já esta com um livro de entrega pendente.')
+
+                   
