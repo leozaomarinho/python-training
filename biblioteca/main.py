7cdad22c4a4c931584ebabaf19a0af1a5f2eb34b
Leonardo Marinho <81639502+leozaomarinho@users.noreply.github.com>
2023-12-15 18:25:28 -0300

    refatorando classes e metodos

--- biblioteca/main.py
+++ biblioteca/main.py
@@ -1,20 +1,34 @@
-import library
-import solicitante
+from library import Library
+from solicitante import solicitante
+from livro import Livro
 
-biblioteca =library()
+
+
+biblioteca =Library()
 
 nome_solicitante = input('Qual o seu nome: ')
 pendencia = False
 
 locatario = solicitante(nome_solicitante,pendencia)
 
-print('O que você deseja fazer: ')
-print('1 - pegar livro emprestado.')
-print('2 - listar livros disponiveis.')
-print('3 - devolver livro.')
-print('0 - sair.')
+while True:
 
-opcao = input('Digite o numero da opcao desejada (0 a 3): ')
+    print('O que você deseja fazer: ')
+    print('1 - pegar livro emprestado.')
+    print('2 - listar livros disponiveis.')
+    print('3 - devolver livro.')
+    print('4 - cadastrar livro.')
+    print('0 - sair.')
 
-if opcao ==1:
+    opcao = input('Digite o numero da opcao desejada (0 a 3): ')
+    
+    if opcao ==1:
+        biblioteca.solicitar_livro(locatario,)
+        
+    elif opcao ==4:
+        biblioteca
+    
+    
+
+
     
--- biblioteca/solicitante.py
+++ biblioteca/solicitante.py
@@ -1,8 +1,9 @@
-class solicitante:
+class Solicitante:
     def __init__(self,nome):
         self.nome=nome
         self.livro_pend = None
         
     def __str__(self) -> str:
         return f'Solicicitante {self.nome}. livro pendente: ({'Possui pendencia' if self.livro_pend == True else 'Nao possui pendencia'})'
+    
         
