9c29f34390c2abc819767362b4c446832f0a30ee
Leonardo Marinho <leowalkg6@gmail.com>
2024-01-04 17:31:19 -0300

    adicionando testes

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,23 @@
 from src.livro import Livro
+from src.solicitante import Solicitante
 import pytest 
 
 @pytest.fixture
 def livro ():
     return Livro("harry poter","ficcao",True)
 
-def test_livro(livro):
-    if livro is type(Livro):
-        assert True
+@pytest.fixture
+def solicitante ():
+    return Solicitante("Leonardo")
+class TestLivro:
+    def test_tipo_livro(livro):
+        if livro is type(Livro):
+            assert True
+            
+    def test_livro_disp(livro):
+        assert livro == livro
+        
+class TestSolicitante:
+    def test_valida_solicitante(solicitante):
+        soli = Solicitante("Leonardo")
+        assert soli.nome == "Leonardo"
